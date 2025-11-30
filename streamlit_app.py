65a364b64ba1d38ef83547b6869a7b0400d20d07
tinabu2006-cell <tinabu2006@gmail.com>
2025-11-30 21:43:49 +1100

    Update streamlit_app.py
    search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write ('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width = True)
 
 ingredients_list = st.multiselect (
@@ -33,7 +33,7 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
+        st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
